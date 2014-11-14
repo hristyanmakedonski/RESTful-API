bfcd70f91e3aa48785c6901f3521e5107b8dbca9
hristyanmakedonski <h.makedonski@abv.bg>
2014-11-15 00:08:52 +0200

    Added database backup and config file

--- db.sql
+++ db.sql
@@ -1,73 +1,54 @@
--- phpMyAdmin SQL Dump
--- version 4.1.12
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Nov 13, 2014 at 04:46 PM
--- Server version: 5.6.16
--- PHP Version: 5.5.11
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `restful`
---
-CREATE DATABASE IF NOT EXISTS `restful` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `restful`;
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.5.24-log - MySQL Community Server (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL Version:             8.3.0.4792
 -- --------------------------------------------------------
 
---
--- Table structure for table `candidates`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Dumping database structure for restful
+CREATE DATABASE IF NOT EXISTS `restful` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `restful`;
+
+
+-- Dumping structure for table restful.candidates
 CREATE TABLE IF NOT EXISTS `candidates` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   `position` varchar(100) DEFAULT NULL,
   `created_on` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `candidates`
---
-
+-- Dumping data for table restful.candidates: ~3 rows (approximately)
+/*!40000 ALTER TABLE `candidates` DISABLE KEYS */;
 INSERT INTO `candidates` (`id`, `name`, `position`, `created_on`) VALUES
-(1, 'Ivan', 'PHP Developer', '2014-11-13 15:06:47'),
-(2, 'George', 'Java Developer', '2014-11-13 15:06:58'),
-(3, 'Petar ', 'JavaScript Developer', '2014-11-13 15:07:15');
+	(1, 'Ivan', 'PHP Developer', '2014-11-13 15:06:47'),
+	(2, 'George', 'Java Developer', '2014-11-13 15:06:58'),
+	(3, 'Petar ', 'JavaScript Developer', '2014-11-13 15:07:15');
+/*!40000 ALTER TABLE `candidates` ENABLE KEYS */;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `jobs`
---
-
+-- Dumping structure for table restful.jobs
 CREATE TABLE IF NOT EXISTS `jobs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `position` varchar(50) NOT NULL DEFAULT '0',
   `description` varchar(200) NOT NULL DEFAULT '0',
   `created_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `jobs`
---
-
+-- Dumping data for table restful.jobs: ~3 rows (approximately)
+/*!40000 ALTER TABLE `jobs` DISABLE KEYS */;
 INSERT INTO `jobs` (`id`, `position`, `description`, `created_on`) VALUES
-(1, 'Java Developer', 'Android, Swing ...', '2014-11-13 15:06:47'),
-(2, 'PHP Developer', 'Mysql, Apache ..etc', '2014-11-13 15:06:47'),
-(3, 'JavaScript Developer', 'jquery, nodejs ..etc', '2014-11-13 15:06:47');
-
+	(1, 'Java Developer', 'Android, Swing ...', '2014-11-13 15:06:47'),
+	(2, 'PHP Developer', 'Mysql, Apache ..etc', '2014-11-13 15:06:47'),
+	(3, 'JavaScript Developer', 'jquery, nodejs ..etc', '2014-11-13 15:06:47');
+/*!40000 ALTER TABLE `jobs` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
