ab62f56d4b3a53fd4382df75e974ae52d0a66891
hristyanmakedonski <h.makedonski@abv.bg>
2014-11-14 12:10:24 +0200

    //this is rest.php (index)

---
+++ rest.php
@@ -0,0 +1,36 @@
+
+<?php 
+require "./config.php";
+
+$url = trim($_REQUEST['url']);
+$url = explode('/', $url);
+$Object = trim($url[0]);
+$Action = trim($url[1]);
+
+$requestType = $_SERVER['REQUEST_METHOD'];
+
+if( empty($Object) )  die; 
+
+if( !class_exists($Object) ) die; 
+$Handle = new $Object();
+
+if($Action==='list'){ 
+	$Action.='ing';
+	if($requestType !== 'GET') die;
+	$Handle->$Action();	 
+}else if($Action ==='review' || $Action === 'search'){
+	if(key_exists(2,$url) && is_numeric($url[2]) ) {
+
+		if(!method_exists($Object, $Action) )die;
+
+		$Handle->$Action( $url[2] );	
+	}
+	
+}else if( !key_exists(3,$url)  && is_numeric( $url[1]) ){
+		$Action = 'index';
+		$Handle->$Action($url[1]);   
+}
+
+
+
+?>
